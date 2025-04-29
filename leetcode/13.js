ece9040c698266d16518ff0efb31044c5b5ec687
Hari krishnan P U <harikrishnan9a@gmail.com>
2025-04-29 21:50:52 +0530

    leet code 13

--- leetcode/13.js
+++ leetcode/13.js
@@ -1,49 +1,44 @@
+//WEEK 10
+
 /**
  * @param {string} s
  * @return {number}
  */
-var romanToInt = function(s) {
+var romanToInt = function(s) { // LCM
     let val = 0;
-    let reversedOrder = s.split('').reverse().join('');
-    for(let v of reversedOrder){
-        switch(v){
-
-            case "I":
-            val += 1;
-            break;
-
-            case "V":
-            val += 5;
-            break;
-
-            case "X":
-            val += 10;
-            break;
-
-            case "L":
-            val += 50;
-            break;
-
-            case "C":
-            val += 100;
-            break;
-
-            case "D":
-            val += 500;
-            break;
-
-            case "M":
-            val += 1000;
-            break;
-
-            default:
-            val += 0;
-
-        }
-
-
-
+    let arr = s.split('');
+    let roman = {
+        'I' : 1,
+        'V' : 5,
+        'X' : 10,
+        'L' : 50,
+        'C' : 100,
+        'D' : 500,
+        'M' : 1000
     }
     
-        return val
-};
+    let value = 0;
+        
+
+    for(let i=0; i < arr.length; i++){
+        
+
+        for(let r in roman){
+            if(arr[i] == r){
+                if(value < roman[r]){
+                    value  = roman[r] - value
+                }else{
+                    value += roman[r]
+                }
+            }
+        }
+        
+    }
+    
+    return value
+    
+    
+}
+
+
+console.log(romanToInt("V"));
